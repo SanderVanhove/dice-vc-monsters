f82cc5884a57ccd7a90ac01cb6ad91571bfc669e
Sander Vanhove <sandervhove@gmail.com>
2021-06-07 02:31:10 +0200

    Add music and spel check

--- src/Screens/Title/Title.gd
+++ src/Screens/Title/Title.gd
@@ -4,3 +4,8 @@ extends Control
 func _on_RollButton_pressed() -> void:
 	$ClickAudio.play()
 	get_tree().change_scene("res://Screens/Main/Main.tscn")
+
+
+func _on_Label3_gui_input(event: InputEvent) -> void:
+	if event as InputEventMouseButton and not event.is_pressed():
+		OS.shell_open("https://twitter.com/SanderVhove")
